32ee59a8470f5dc5eab609e711427dc0a0cf0baa
Shravan Chopra <shrav_20@hotmail.com>
2017-04-25 20:01:33 -0500

    created basic layout and set up the pokeCell class

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Shravan Chopra on 25/04/17.
+//  Copyright © 2017 Shravan Chopra. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    var pokemon: Pokemon!
+    
+    // create the pokemon from data
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        // update cell UI
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Shravan Chopra on 24/04/17.
+//  Copyright © 2017 Shravan Chopra. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+
+    private var _name: String!
+    private var _pokedexID: Int!
+
+    // creating getters
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init (name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
